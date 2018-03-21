d9a569d5dc45cfb84e85c0a7d4b86b2583c5a3db
Adibas03 <adibas03@ymail.com>
2018-03-21 11:58:33 +0100

    clean up keen-analysis code

--- cli/analytics.js
+++ cli/analytics.js
@@ -1,4 +1,3 @@
-const KeenAnalysis = require('keen-analysis');
 const KeenTracking = require('keen-tracking');
 
 const COLLECTIONS = {
@@ -13,13 +12,11 @@ class Analytics {
     constructor(web3) {
         this.projectId = process.env.KEEN_PROJECT_ID;
         this.writeKey = process.env.KEEN_WRITE_KEY;
-        this.readKey = process.env.KEEN_READ_KEY;
 
         this._web3 = web3;
 
         this.activeNodes = 0;
         this.networkId = null;
-        this.analysisClient = null;
         this.trackingClient = null;
         this.initialize();
     }
@@ -36,11 +33,6 @@ class Analytics {
     initialize() {
         this.getActiveNetwork();
 
-        this.analysisClient = new KeenAnalysis({
-            projectId: this.projectId,
-            readKey: this.readKey
-        });
-
         this.trackingClient = new KeenTracking({
             projectId: this.projectId,
             writeKey: this.writeKey
@@ -48,7 +40,7 @@ class Analytics {
     }
 
     async awaitInitialized() {
-        if (!this.analysisClient || !this.trackingClient || !this.networkId) {
+        if (!this.trackingClient || !this.networkId) {
             return new Promise((resolve) => {
                 setTimeout(async () => {
                     resolve(await this.awaitInitialized());
@@ -63,17 +55,12 @@ class Analytics {
         await this.awaitInitialized();
         nodeAddress = this._web3.sha3(nodeAddress);
         this.notifyNetworkNodeActive(nodeAddress);
-        this.pollActiveClinodesCount();
     }
 
     stopAnalytics() {
         if (this.notifyInterval) {
             clearInterval(this.notifyInterval);
             this.notifyInterval = null;
-        }
-        if (this.pollInterval) {
-            clearInterval(this.pollInterval);
-            this.pollInterval = null;
         }
     }
 
@@ -90,36 +77,6 @@ class Analytics {
     notifyNetworkNodeActive(nodeAddress) {
         this.sendActiveTimeNodeEvent(nodeAddress)
         this.notifyInterval = setInterval(() => this.sendActiveTimeNodeEvent(nodeAddress), ACTIVE_CLINODES_POLLING_INTERVAL);
-    }
-
-    getActiveClinodesCount(networkId) {
-        const count = new KeenAnalysis.Query('count', {
-            event_collection: COLLECTIONS.TIMENODES,
-            target_property: 'nodeAddress',
-            timeframe: 'previous_2_minutes',
-            filters: [
-                {
-                    property_name: 'networkId',
-                    operator: 'eq',
-                    property_value: networkId
-                },
-                {
-                    property_name: 'status',
-                    operator: 'eq',
-                    property_value: 'active'
-                }
-            ]
-        });
-
-        this.analysisClient.run(count, (err, response) => {
-            this.activeNodes = response.result;
-        });
-    }
-
-    async pollActiveClinodesCount() {
-        await this.getActiveClinodesCount(this.networkId);
-
-        this.pollInterval = setInterval(() => this.getActiveClinodesCount(this.networkId), ACTIVE_CLINODES_POLLING_INTERVAL);
     }
 }
 
