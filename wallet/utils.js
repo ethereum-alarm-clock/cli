52e1eb578415a01c9cd5da625412701cbee616e7
Lsaether <Lsaether@protonmail.com>
2018-06-22 15:46:19 +0300

    intermittent commit

--- wallet/utils.js
+++ wallet/utils.js
@@ -1,8 +1,9 @@
 const { Wallet } = require('eac.js-client')
+const fs = require('fs');
 
 const loadWalletFromKeystoreFile = function(web3, fileName, password) {
     const wallet = new Wallet(web3)
-    const file = fs.readFileSync(fileName, 'utf-8')
+    const file = fs.readFileSync(fileName[0], 'utf-8')
     const keystore = JSON.parse(file)
     wallet.decrypt(keystore, password)
 
